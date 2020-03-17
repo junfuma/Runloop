f0c24c7bd02ed62da5c9714fa3b5c0dd10e8160a
Alex <shuting5921@sina.cn>
2020-03-17 21:24:25 +0800

    new

---
+++ 02-Runloop与线程的关系/customerThread.h
@@ -0,0 +1,11 @@
+
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface customerThread : NSThread
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ 02-Runloop与线程的关系/customerThread.m
@@ -0,0 +1,9 @@
+
+
+#import "customerThread.h"
+
+@implementation customerThread
+- (void)dealloc{
+    NSLog(@"%s---线程销毁了",__func__);
+}
+@end
